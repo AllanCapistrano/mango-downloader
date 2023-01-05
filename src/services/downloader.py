8f34dd06e7668e2927aca6ff6b5f49f5e87324d3
AllanCapistrano <allan.capistrano3@gmail.com>
2023-01-05 11:53:38 -0300

    Refactor: remove createDirectory and moveFiles private methods

--- src/services/downloader.py
+++ src/services/downloader.py
@@ -1,7 +1,9 @@
 from typing import List
-from os import system
 
 from command import run
+
+from modules.terminal import createDirectory
+from modules.terminal import moveFiles
 
 # ------------------------------ Constants ----------------------------------- #
 MANGA_NAME_INDEX = 5
@@ -10,7 +12,7 @@ FILE_EXTENSION_INDEX = 7
 # ---------------------------------------------------------------------------- #
 
 class Downloader:
-    def downloadImage(self, urls: List[str]) -> None:
+    def downloadImages(self, urls: List[str]) -> None:
         """ Baixa as imagens a partir de uma URL, e coloca no diretório 
         `mangas/<nome do mangá>/<capítulo do mangá>`.
 
@@ -24,7 +26,7 @@ class Downloader:
             temp: List[str] = urls[0].split("/")
             directory_name: str = "mangas/" + temp[MANGA_NAME_INDEX] + "/" + temp[MANGA_CHAPTER_INDEX]
 
-            if(self.__createDirectory__(directory_name=directory_name)):
+            if(createDirectory(directory_name=directory_name)):
                 command: List[str] = ["curl"]
                 
                 for url in urls:
@@ -35,9 +37,9 @@ class Downloader:
                     run(command=command)
                     
                     directory_name: str = "mangas/'" + temp[MANGA_NAME_INDEX] + "'/" + temp[MANGA_CHAPTER_INDEX]
-                    image_extension: str= temp[FILE_EXTENSION_INDEX][-4:]
+                    image_extension: str = temp[FILE_EXTENSION_INDEX][-4:]
 
-                    self.__moveFiles__(
+                    moveFiles(
                         file_extension=image_extension, 
                         directory_name=directory_name
                     )
@@ -48,43 +50,3 @@ class Downloader:
                     exit()
         else:
             print("Este capítulo não possui páginas para serem baixadas!")
-
-    def __createDirectory__(self, directory_name: str) -> bool:
-        """ Cria um diretório a partir do caminho especificado.
-        
-        Parameters
-        ----------
-        directory_name: :class:`str`
-            Nome do diretório juntamente com o caminho.
-        """
-        
-        try:
-            run(command=["mkdir", "-p", directory_name])
-
-            return True
-        except:
-            print("Erro ao criar o diretório: " + directory_name)
-
-            return False
-
-    def __moveFiles__(self, file_extension: str, directory_name: str) -> bool:
-        """ Move todos os arquivos de uma determinada extensão, para o diretório
-        especificado.
-
-        Parameters
-        ----------
-        file_extension: :class:`str`
-            Extensão do arquivo.
-        directory_name: :class:`str`
-            Nome do diretório juntamente com o caminho.
-
-        """
-        
-        file_extension: str = "*" + file_extension
-
-        try:
-            system("mv " + file_extension + " " + directory_name)
-
-            return True
-        except:
-            return False
