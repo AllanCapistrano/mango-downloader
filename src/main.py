33fd2730281cad09d3449bd8728486640e8b51d3
AllanCapistrano <allan.capistrano3@gmail.com>
2023-01-09 11:00:56 -0300

    Feat: Uploading and removing files

--- src/main.py
+++ src/main.py
@@ -1,21 +1,30 @@
 from typing import List
 
-from services.crawler import Crawler
-from services.downloader import Downloader
+from services import Crawler
+from services import Downloader
+from services import Uploader
 from modules.unionLeitor import parser
 from modules.terminal.zip import zip
+from modules.terminal.removeFiles import removeFilesAndDirectories
 
 if __name__ == "__main__":
     print("Bem vindo(a) ao Mango Downloader!")
     chapter_link = input("Digite a URL do capítulo que deseja baixar: ")
 
     if(parser.idValidUrl(chapter_link)):
-        crawler = Crawler()
-        downloader = Downloader()
+        crawler: Crawler = Crawler()
+        downloader: Downloader = Downloader()
+        uploader: Uploader = Uploader()
 
         manga_pages: List[str] = crawler.getMangaImageUrls(chapter_link)
 
         downloader.downloadImages(manga_pages)
-        zip()
+        compressed_file: str = zip()
+
+        if(compressed_file != None):
+            was_uploaded: bool = uploader.upload([compressed_file])
+
+            if(was_uploaded):
+                removeFilesAndDirectories()
     else:
         print("Error! URL inválida, tente novamente.")
