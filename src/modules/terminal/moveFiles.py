acc69315c5cd8e119db0098a2033876741109386
AllanCapistrano <allan.capistrano3@gmail.com>
2023-01-09 11:09:01 -0300

    Style: fix identation

--- src/modules/terminal/moveFiles.py
+++ src/modules/terminal/moveFiles.py
@@ -1,23 +1,23 @@
 from os import system
 
 def moveFiles(file_extension: str, directory_name: str) -> bool:
-        """ Move todos os arquivos de uma determinada extensão, para o diretório
-        especificado.
+    """ Move todos os arquivos de uma determinada extensão, para o diretório
+    especificado.
 
-        Parameters
-        ----------
-        file_extension: :class:`str`
-            Extensão do arquivo.
-        directory_name: :class:`str`
-            Nome do diretório juntamente com o caminho.
+    Parameters
+    ----------
+    file_extension: :class:`str`
+        Extensão do arquivo.
+    directory_name: :class:`str`
+        Nome do diretório juntamente com o caminho.
 
-        """
-        
-        file_extension: str = "*" + file_extension
+    """
+    
+    file_extension: str = "*" + file_extension
 
-        try:
-            system("mv " + file_extension + " " + directory_name)
+    try:
+        system("mv " + file_extension + " " + directory_name)
 
-            return True
-        except:
-            return False
+        return True
+    except:
+        return False
