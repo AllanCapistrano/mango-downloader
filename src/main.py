7cc40f16533ff6ec25bf0f77929c19eed240f7f6
AllanCapistrano <allan.capistrano3@gmail.com>
2023-01-10 15:42:27 -0300

    Feat: allows to download more than one chapter at a time

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Dict
+from os import system
 
 from services import Crawler
 from services import Downloader
@@ -9,22 +10,60 @@ from modules.terminal.removeFiles import removeFilesAndDirectories
 
 if __name__ == "__main__":
     print("Bem vindo(a) ao Mango Downloader!")
-    chapter_link = input("Digite a URL do capítulo que deseja baixar: ")
 
-    if(parser.idValidUrl(chapter_link)):
-        crawler: Crawler = Crawler()
-        downloader: Downloader = Downloader()
-        uploader: Uploader = Uploader()
+    chapter_link: str = input("Digite a URL do capítulo que deseja baixar: ")
+    chapters: List[Dict[str, bool]] = [
+        {
+            "chapter_link": chapter_link,
+            "downloaded": False
+        }
+    ]
 
-        manga_pages: List[str] = crawler.getMangaImageUrls(chapter_link)
+    option: str = input("Deseja baixar mais algum capítulo (S/n)? ")
 
-        downloader.downloadImages(manga_pages)
-        compressed_file: str = zip()
+    while(option != "N" and option != "n"):
+        if(option != "S" and option != "s"):
+            print("Opção inválida! Tente novamente.")
 
-        if(compressed_file != None):
-            was_uploaded: bool = uploader.upload([compressed_file])
+        chapter_link: str = input("Digite a URL do capítulo que deseja baixar: ")    
+        chapters.append(
+            {
+                "chapter_link": chapter_link,
+                "downloaded": False
+            }
+        )
 
-            if(was_uploaded):
-                removeFilesAndDirectories()
-    else:
-        print("Error! URL inválida, tente novamente.")
+        option: str = input("Deseja baixar mais algum capítulo (S/n)? ")
+
+    system("clear") # Limpando o terminal
+
+    # Fazendo o download dos capítulos.
+    for chapter in chapters:
+        if(parser.idValidUrl(chapter["chapter_link"])):
+            crawler: Crawler = Crawler()
+            downloader: Downloader = Downloader()
+            uploader: Uploader = Uploader()
+
+            manga_pages: List[str] = crawler.getMangaImageUrls(
+                chapter["chapter_link"]
+            )
+
+            chapter["downloaded"] = downloader.downloadImages(manga_pages)
+        else:
+            chapter["downloaded"] = False
+
+    # Verificando se todos os capítulos foram baixados com sucesso.
+    for chapter in chapters:
+        if(not chapter["downloaded"]):
+            print("Não foi possível baixar o capítulo do link: " 
+                + chapter["chapter_link"])
+    
+    # Comprimindo os arquivos.
+    compressed_file: str = zip()
+
+    # Fazendo o upload dos arquivos.
+    if(compressed_file != None):
+        was_uploaded: bool = uploader.upload([compressed_file])
+
+        if(was_uploaded):
+            removeFilesAndDirectories()
