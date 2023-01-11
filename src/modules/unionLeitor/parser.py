dd3ad595537886f7377741179e077930404e67e1
AllanCapistrano <allan.capistrano3@gmail.com>
2023-01-11 16:37:39 -0300

    Feat: add isMangaLink function

--- src/modules/unionLeitor/parser.py
+++ src/modules/unionLeitor/parser.py
@@ -83,3 +83,21 @@ def isNotBanner(url: str) -> bool:
         return False
 
     return True
+
+def isMangaLink(url: str) -> bool:
+    """ Verifica se a URL informada é de um mangá ou não.
+
+    Parameters
+    ----------
+    url: :class:`str`
+        URL que deseja verificar.
+
+    Returns
+    -------
+    :class:`bool`
+    """
+
+    if(url.find("manga") != -1):
+        return True
+
+    return False
