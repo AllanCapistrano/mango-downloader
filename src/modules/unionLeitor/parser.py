383a324e4de934d59bc84432e8f3316a90ed5359
AllanCapistrano <allan.capistrano3@gmail.com>
2023-01-16 21:40:23 -0300

    Feat: add isUrl function

--- src/modules/unionLeitor/parser.py
+++ src/modules/unionLeitor/parser.py
@@ -1,5 +1,26 @@
 from typing import List
 
+def isUrl(url: str) -> bool:
+    """ Verifica se o que foi passado é uma URL ou não.
+
+    Parameters
+    ----------
+    url: :class:`str`
+        URL que deseja verificar.
+
+    Returns
+    -------
+    :class:`bool`
+    """
+
+    valid_urls: List[str] = ["https://", "http://"]
+
+    for valid_url in valid_urls:
+        if(url.find(valid_url) != -1):
+            return True
+
+    return False
+
 def isValidUrl(url: str) -> bool:
     """ Verifica se a URL passada é de uma página do site `unionleitor.top/`.
 
